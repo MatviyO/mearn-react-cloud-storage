dede1406fb1225b9370bc40b627ec0cdd3b8477b
Oleh <oleh.m97@gmail.com>
2021-05-30 21:42:53 +0300

    up

--- client/my-app/src/components/animations/Animations.tsx
+++ client/my-app/src/components/animations/Animations.tsx
@@ -1,10 +1,23 @@
-import React, {FC, useState} from 'react'
+import React, {FC, useEffect, useState} from 'react'
 import './animation.scss'
-type Props = {
-}
+import {CSSTransition, SwitchTransition, Transition} from "react-transition-group";
+
+type Props = {}
 
 const Animations: FC<Props> = () => {
     const [loaderVisible, setLoaderVisible] = useState(false)
+    const [mode, setMode] = useState("out-in")
+    const [toggle, setTogle] = useState(false)
+
+    useEffect(() => {
+        setTimeout(() => setLoaderVisible(!loaderVisible), 1000)
+        setTimeout(() => setLoaderVisible(!loaderVisible), 3000)
+    }, []);
+
+    function changeHandler(e: React.ChangeEvent<HTMLInputElement>) {
+        setMode(e.target.value)
+    }
+
     return (
         <div className="container">
             <div className="row">
@@ -12,10 +25,21 @@ const Animations: FC<Props> = () => {
                     <div className="disk">
                         <div className="disk__btns">
                             <div className="disk__upload ms-2">
-                                <button type="button" className="btn btn-dark"
-                                    onClick={() => setLoaderVisible(!loaderVisible)}
-                                    >{loaderVisible ? "Hide" : 'Show'}
-                                </button>
+
+                                <SwitchTransition mode={mode as "out-in" | "in-out"}>
+                                    <CSSTransition
+                                        key={toggle as any}
+                                        classNames={"fade"}
+                                        timeout={500}>
+                                        <button className="btn btn-dark" onClick={() => {
+                                            setTogle(!toggle)
+                                            setLoaderVisible(!loaderVisible)
+                                        }}>
+                                            {loaderVisible ? "Hide" : 'Show'}
+
+                                        </button>
+                                    </CSSTransition>
+                                </SwitchTransition>
                             </div>
                         </div>
                     </div>
@@ -23,7 +47,15 @@ const Animations: FC<Props> = () => {
 
                 <div className="col-12">
                     <div className="wrap">
-                        { loaderVisible &&  <div className="circle"/>}
+                        <Transition
+                            in={loaderVisible}
+                            timeout={500}
+                            mountOnEnter
+                            unmountOnExit
+                        >
+                            {state => <div className={`circle ${state}`}/>}
+
+                        </Transition>
                     </div>
                 </div>
             </div>
--- client/my-app/src/components/disk/DIsk.tsx
+++ client/my-app/src/components/disk/DIsk.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent, FC, useEffect} from 'react'
+import React, {FC, useEffect, useState} from 'react'
 import {useDispatch, useSelector} from "react-redux";
 import {IStateReducer} from "../../interfaces/IStateReducer";
 import {getFiles, uploadFile} from "../../services/file";
@@ -7,13 +7,13 @@ import {setCurrentDir, setPopupDisplay} from "../../redux/action/fileAction";
 import Popup from "./Popup/Popup";
 import './disk.scss'
 
-type Props = {
-}
+type Props = {}
 
 const Disk: FC<Props> = () => {
     const dispatch = useDispatch()
     const currentDir = useSelector((state: IStateReducer) => state.files.currentDir)
     const dirDisk = useSelector((state: IStateReducer) => state.files.dirStack)
+    const [dragEnter, setDragEnter] = useState(false)
 
     useEffect(() => {
         dispatch(getFiles(currentDir))
@@ -21,24 +21,44 @@ const Disk: FC<Props> = () => {
 
     function createander() {
         dispatch(setPopupDisplay('flex'))
-
     }
+
     function backClickHandler() {
         const backDirId = dirDisk.pop()
         dispatch(setCurrentDir(backDirId as string))
     }
+
     function handlerFileup(files: any) {
         dispatch(uploadFile(files[0], currentDir))
+    }
+    const onDragEnterHandler = (e: React.DragEvent<HTMLDivElement>): void => {
+        e.preventDefault()
+        e.stopPropagation()
+        setDragEnter(true)
+    }
+    const onDragLeaveHandler = (e: React.DragEvent<HTMLDivElement>): void => {
+        e.preventDefault()
+        e.stopPropagation()
+        setDragEnter(false)
+    }
+    const dropHandler = (e: React.DragEvent<HTMLDivElement>, filesDrag: any): void  => {
+        e.preventDefault()
+        e.stopPropagation()
+        setDragEnter(false)
+        dispatch(uploadFile(filesDrag[0], currentDir))
     }
 
     return (
         <div className="container">
             <div className="row">
                 <div className="col-12">
-                    <div className="disk">
+                    {!dragEnter ? <div className="disk" onDragEnter={onDragEnterHandler}
+                                        onDragLeave={onDragLeaveHandler}
+                                        // onDragOver={onDragLeaveHandler}
+                        >
                         <div className="disk__btns">
                             {currentDir && <button type="button" className="btn btn-dark me-2"
-                                     onClick={() => backClickHandler()}>Back</button>}
+                                                   onClick={() => backClickHandler()}>Back</button>}
                             <button type="button" className="btn btn-dark" data-bs-toggle="modal"
                                     data-bs-target="#exampleModal" onClick={() => createander()}>Create
                             </button>
@@ -52,6 +72,15 @@ const Disk: FC<Props> = () => {
                         <FileList/>
                         <Popup/>
                     </div>
+                   :
+                        <div className="drag-area" onDragEnter={onDragEnterHandler}
+                             onDrop={(e) => dropHandler(e, e.dataTransfer.files)}
+                             onDragLeave={onDragLeaveHandler}
+                             // onDragOver={onDragLeaveHandler}
+                        >
+                            Move files here
+                        </div>
+                   }
                 </div>
             </div>
         </div>
